3df98ef6d458b9d2138125d852c87bfc655156f0
sangs <sangs8448@gmail.com>
2020-12-11 23:51:31 +0900

    시작화면 구성

--- Attendance/AppDelegate.swift
+++ Attendance/AppDelegate.swift
@@ -13,10 +13,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
+    var wondiw: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
         FirebaseApp.configure()
+        sleep(1)
         return true
     }
 
